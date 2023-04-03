82b9175347beb02c3d469350efbf512380dcac44
ggldnl <danielgigliotti99.dg@gmail.com>
2023-04-03 10:50:40 +0200

    refactor comments

--- homework_2.sql
+++ homework_2.sql
@@ -2,13 +2,13 @@
 /* ------------------------------ foreign keys ------------------------------ */
 
 alter table country_territories
-	add constraint fk_alpha_3_ct foreign key (alpha_3) references world_countries (alpha_3)
+	add constraint fk_alpha_3_ct foreign key (alpha_3) references world_countries (alpha_3);
 
 alter table eu_countries
-	add constraint fk_alpha_3_eu foreign key (alpha_3) references world_countries (alpha_3)
+	add constraint fk_alpha_3_eu foreign key (alpha_3) references world_countries (alpha_3);
 
 alter table co2_emissions
-	add constraint fk_alpha_3_co2 foreign key (alpha_3) references world_countries (alpha_3)
+	add constraint fk_alpha_3_co2 foreign key (alpha_3) references world_countries (alpha_3);
 
 /* ----------------------------- world countries ---------------------------- */
 
@@ -20,7 +20,6 @@ alter table world_countries
     alter column population2020 set not null,
     alter column density set not null,
     alter column land_area set not null,
-    alter column med_age set not null,
     alter column region set not null;
 	
 /* --------------------------- country territories -------------------------- */
@@ -48,7 +47,7 @@ alter table eu_countries
 /* ------------------------------ co2 emissions ----------------------------- */
 
 alter table co2_emissions
-	add constraint unique_country_co2 unique (country);
+	add constraint unique_country_co2 unique (country, ref_year);
 	
 alter table co2_emissions
 	alter column country set not null;
@@ -56,10 +55,10 @@ alter table co2_emissions
 /* --------------------------------- indexes -------------------------------- */
 
 /* Index selectivity -> create index on columns that are common in WHERE, 
--- ORDER BY and GROUP BY clauses. 
--- We may consider adding an index in colums that are used to relate other tables (through a JOIN, for example)
--- and on values that have big domains, i.e. names, etc. Don't create indexes on columns with small variability,
--- like Male/Female columns.
+-- ORDER BY and GROUP BY clauses. We may consider adding an index in colums 
+-- that are used to relate other tables (through a JOIN, for example)
+-- and on values that have big domains, i.e. names, etc. Don't create indexes 
+-- on columns with small variability, like Male/Female columns.
 */
 
 create index country_index_wc
@@ -69,7 +68,7 @@ create index population_index_wc
 on world_countries (population2020);
 
 create index total_emissions_index_co2
-on co2_emissions (total)
+on co2_emissions (total);
 
 /* --------------------------------- queries -------------------------------- */
 
